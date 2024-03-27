1b826bb4b19734a40561cc5c9466e65d2da1ea43
baalaji03 <rps123a@gmail.com>
2024-03-27 02:19:53 -0700

    Roadmap5

---
+++ readme.md
@@ -0,0 +1,2 @@
+Day 5 task is about creating our own resume data in JSON Format.
+And with that JSON I did iterate with all for loops like for , for in , for of and foreach.
---
+++ script.js
@@ -0,0 +1,34 @@
+var resume = {
+       
+    name:"RPS Baalaji",
+    Location:"pondy",
+    Qualification:"B.Tech (IT)",
+    LanguageKnow:"tamil,ENGLISH",
+    SKILLS: "JS",
+    AreaofInterest:["computer nteworks","OOP","software engineering"],
+    Mobilenumber : 9597678766, 
+};
+
+// for loop:
+// for(var i=0;i<resume.AreaofInterest.length;i++)
+// {
+// console.log(resume.AreaofInterest[i]);
+// }
+
+//for in:
+// for(key in resume)
+// {
+// console.log(key); //console.log(resume[key]); 
+// }
+
+
+//for of:
+// const data = Object.entries(resume);
+// for(var [key,value] of data)
+// {
+//     console.log(key);  // console.log(value);
+// }
+
+//for each:
+// const data = Object.entries(resume);
+// data.forEach(([key,value])=>console.log(value)); // data.forEach(([key,value])=>console.log(key));
